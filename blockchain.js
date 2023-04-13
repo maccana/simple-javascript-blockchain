ee9bccb7f2d1a225610c7a2e15af6e18fe2de373
IT-ACA <mikeymccann@gmail.com>
2023-04-13 15:56:56 -0400

    Moved Block class to own module

--- blockchain.js
+++ blockchain.js
@@ -1,22 +1,5 @@
 const SHA256 = require("crypto-js/sha256");
-
-class Block {
-    constructor(index, timestamp, data, previousHash = "") {
-        this.index = index;
-        this.data = data;
-        this.timestamp = timestamp;
-        this.previousHash = previousHash;
-        this.hash = this.calculateHash()
-    }
-    calculateHash() {
-        return SHA256(
-            this.index +
-            this.previousHash +
-            this.timestamp +
-            JSON.stringify(this.data).toString()
-        )
-    }
-}
+const Block = require("./Block.js")
 
 class Blockchain {
     constructor() {
@@ -46,7 +29,9 @@ myBlockchain.addBlock(new Block(1, "4/12/2023", { msg: 'Send 3000 taytoCoin to J
 myBlockchain.addBlock(new Block(2, "2/20/2022", { msg: 'hello Jube!' }));
 myBlockchain.addBlock(new Block(3, "6/10/2021", { msg: 'No taytoCoins left' }));
 
-const genBlock = myBlockchain.getFullBlockchain()
+const fullBlockchain = myBlockchain.getFullBlockchain()
 const singleBlock = myBlockchain.getSingleBlock(1)['data']['msg']
 console.log(JSON.stringify(singleBlock, null, 4));
+console.log(JSON.stringify(fullBlockchain, null, 4));
+
 
